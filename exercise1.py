4268201dd6b4bed456ac21ee755e68e0bc917d9d
Jakehatesdyl <jaketmarsden@icloud.com>
2025-01-12 15:25:51 +0000

    gender

--- exercise1.py
+++ exercise1.py
@@ -1,15 +1,18 @@
 myage = None
-myname=input("what is your name?")
+myname=input("what is your name? ")
 
 while myage is None:
-    inputmyage=input("what is your age?")
+    inputmyage=input("what is your age? ")
     try:
         myage=int(inputmyage)
     except ValueError:
-        print(inputmyage,"is not a digits, please enter digits only")
+        print(inputmyage,"is not a digit, please enter digits only")
 
+gender = None
+while gender not in {"male", "female" ,"non binary"}:
+    gender = input ("Do you identify as male,female or non binary? ").lower()
 
 for i in range(150):
     # print(i)
     if i==myage:
-        print("Your name is",myname,"and Your age is",i)
+        print(f"Your name is {myname}, your age is {i} and you identify as {gender}.")
